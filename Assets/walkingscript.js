e319617aab9ddbe36b67e538d6b01bd9fc371aa6
s0dz <garrett.mctear@gmail.com>
2015-02-04 00:43:56 -0800

    Learning the animations side of Unity
    A little difficult to get the animation you want at first.

---
+++ Assets/walkingscript.js
@@ -0,0 +1,8 @@
+﻿function Update()
+{
+	if( Input.GetKeyDown("w") )
+	{
+		// Plays the walking animation - stops all other animations
+		animation.Play( "walkinganimation",PlayMode.StopAll );
+	}
+}	
